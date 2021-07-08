a468b37c6ea0f2a5f1c6be32ddf6c3557d40892b
Brady Grapentine <82343457+bradygrapentine@users.noreply.github.com>
2021-07-08 15:39:57 -0400

    Create Easy_MinimumWaitingTime.cs

---
+++ Easy_MinimumWaitingTime.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Linq;
+
+public class Program {
+
+	public int MinimumWaitingTime(int[] queries) {
+		Array.Sort(queries);
+		var result = 0;
+		var timeForLastQuery = 0;
+		for (var i = 1; i < queries.Length; i++) {
+			timeForLastQuery = queries.ToList().GetRange(0,i).Sum();
+			result += timeForLastQuery;
+		}
+		return result;
+	}
+}
