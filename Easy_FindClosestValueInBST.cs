707793d3c8ef9e05f38eee383c536de6525d7a14
Brady Grapentine <82343457+bradygrapentine@users.noreply.github.com>
2021-07-13 20:00:13 -0400

    Update Easy_FindClosestValueInBST.cs

--- Easy_FindClosestValueInBST.cs
+++ Easy_FindClosestValueInBST.cs
@@ -18,8 +18,18 @@ public class Program {
 				{
 					result = n.value ;
 				}
-				queue.Enqueue(n.left);
-                                queue.Enqueue(n.right);
+				if (target < n.value) 
+				{
+					queue.Enqueue(n.left);
+				}
+				else if (target > n.value)
+				{
+					queue.Enqueue(n.right);
+				}
+				else 
+				{
+					break;
+				}
 			}
 		}
 		return result;
@@ -36,5 +46,46 @@ public class Program {
 		}
 	}
 }
+
+
+
+// using System;
+// using System.Collections.Generic;
+
+// public class Program {
+// 	public static int FindClosestValueInBst(BST tree, int target) {
+// 		var result = tree.value;
+//                 var queue = new Queue<BST>();
+// 		if (tree != null) 
+// 		{
+// 			queue.Enqueue(tree);
+// 		}
+//                 while (queue.Count > 0) 
+// 		{
+// 			var n = queue.Dequeue();
+// 			if (n != null) 
+//                         {
+// 				if (Math.Abs(n.value - target) <  Math.Abs(result - target)) 
+// 				{
+// 					result = n.value ;
+// 				}
+// 				queue.Enqueue(n.left);
+//                                 queue.Enqueue(n.right);
+// 			}
+// 		}
+// 		return result;
+// 	}
+
+	
+// 	public class BST {
+// 		public int value;
+// 		public BST left;
+// 		public BST right;
+
+// 		public BST(int value) {
+// 			this.value = value;
+// 		}
+// 	}
+// }
 
 // https://www.algoexpert.io/questions/Find%20Closest%20Value%20In%20BST
