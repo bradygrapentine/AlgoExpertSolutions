401e3c8dfa3160a1f7e9980ca4de96c650fc01f4
Brady Grapentine <82343457+bradygrapentine@users.noreply.github.com>
2021-07-14 21:41:29 -0400

    Update Easy_BinarySearch.cs

--- Easy_BinarySearch.cs
+++ Easy_BinarySearch.cs
@@ -10,7 +10,7 @@ public class Program {
 
 	while(searching) {
 		
-    // ---------------------------------------------- // 
+        // ---------------------------------------------- // 
 		
 		if (array[midpoint] == target) {
 			searching = false;
