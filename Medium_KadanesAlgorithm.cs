d6b93888571ae851a4ef1ce0d097af416aac7e89
Brady Grapentine <82343457+bradygrapentine@users.noreply.github.com>
2021-07-22 19:27:18 -0400

    Update Medium_KadanesAlgorithm.cs

--- Medium_KadanesAlgorithm.cs
+++ Medium_KadanesAlgorithm.cs
@@ -3,15 +3,15 @@ using System.Linq;
 
 public class Program {
 	public static int KadanesAlgorithm(int[] array) {
-    var currentSum = array[0];
-    var maxSum = currentSum;
-    for (var i = 1; i < array.Length; i++)
-    {
-      currentSum = Math.Max(array[i], array[i] + currentSum);
-      maxSum = Math.Max(currentSum, maxSum);
-    }
-    return maxSum;
-  }
+		var currentSum = array[0];
+                var maxSum = currentSum;
+                for (var i = 1; i < array.Length; i++)
+                {
+			currentSum = Math.Max(array[i], array[i] + currentSum);
+			maxSum = Math.Max(currentSum, maxSum);
+		}
+		return maxSum;
+	}
 }
 
 // https://www.algoexpert.io/questions/Kadane's%20Algorithm
