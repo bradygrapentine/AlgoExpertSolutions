d82513cb2f3acf0dd6a2ea6c003de8a4cce1b418
Brady Grapentine <82343457+bradygrapentine@users.noreply.github.com>
2021-08-02 13:14:37 -0400

    Create Easy_CaesarCipherEncryptor.cs

---
+++ Easy_CaesarCipherEncryptor.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Linq;
+using System.Text;
+
+public class Program {
+	public static string CaesarCypherEncryptor(string str, int key) {
+		byte[] asciiBytes = Encoding.ASCII.GetBytes(str);
+		var asciiBytesShifted = asciiBytes
+			.Select(asciiByte => {
+				var shiftedCode = Convert.ToInt32(asciiByte) + key % 26;
+				if (shiftedCode > 122) {
+					shiftedCode = (shiftedCode - 122) + 96;
+				}
+				return (byte)(shiftedCode);
+			})
+			.ToArray();
+		return Encoding.ASCII.GetString(asciiBytesShifted);
+	}
+}
+
+// https://www.algoexpert.io/questions/Caesar%20Cipher%20Encryptor
