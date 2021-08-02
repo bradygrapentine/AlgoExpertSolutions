fa723c6ef5aff3063e581d9f0e76405d3e4dc661
Brady Grapentine <82343457+bradygrapentine@users.noreply.github.com>
2021-08-02 14:13:43 -0400

    Update Easy_CaesarCipherEncryptor.cs

--- Easy_CaesarCipherEncryptor.cs
+++ Easy_CaesarCipherEncryptor.cs
@@ -4,18 +4,36 @@ using System.Text;
 
 public class Program {
 	public static string CaesarCypherEncryptor(string str, int key) {
-		byte[] asciiBytes = Encoding.ASCII.GetBytes(str);
-		var asciiBytesShifted = asciiBytes
-			.Select(asciiByte => {
-				var shiftedCode = Convert.ToInt32(asciiByte) + key % 26;
-				if (shiftedCode > 122) {
-					shiftedCode = (shiftedCode - 122) + 96;
-				}
-				return (byte)(shiftedCode);
-			})
-			.ToArray();
-		return Encoding.ASCII.GetString(asciiBytesShifted);
+		return  Encoding.ASCII.GetString(Encoding.ASCII
+						 .GetBytes(str)
+						 .Select(asciiByte => 
+							 (Convert.ToInt32(asciiByte) + (key % 26)) > 122 ? 
+							 (byte)(((Convert.ToInt32(asciiByte) + (key % 26)) - 122) + 96 ) : 
+							 (byte)(Convert.ToInt32(asciiByte) + (key % 26)))
+						 .ToArray());
 	}
 }
+
+
+// using System;
+// using System.Linq;
+// using System.Text;
+
+// public class Program {
+// 	public static string CaesarCypherEncryptor(string str, int key) {
+// 		byte[] asciiBytes = Encoding.ASCII.GetBytes(str);
+// 		var asciiBytesShifted = asciiBytes
+// 			.Select(asciiByte => {
+// 				var shiftedCode = Convert.ToInt32(asciiByte) + key % 26;
+// 				if (shiftedCode > 122) {
+// 					shiftedCode = (shiftedCode - 122) + 96;
+// 				}
+// 				return (byte)(shiftedCode);
+// 			})
+// 			.ToArray();
+// 		return Encoding.ASCII.GetString(asciiBytesShifted);
+// 	}
+// }
+
 
 // https://www.algoexpert.io/questions/Caesar%20Cipher%20Encryptor
