0b4f52b36b6783713f4ac6804912b98efd17e574
Brady Grapentine <82343457+bradygrapentine@users.noreply.github.com>
2021-08-02 15:01:12 -0400

    Update Easy_FindThreeLargestNumbers.cs

--- Easy_FindThreeLargestNumbers.cs
+++ Easy_FindThreeLargestNumbers.cs
@@ -12,19 +12,19 @@ public class Program {
 }
 
 
-using System;
-using System.Linq;
-using System.Collections.Generic;
+// using System;
+// using System.Linq;
+// using System.Collections.Generic;
 
-public class Program {
-	public static int[] FindThreeLargestNumbers(int[] array) {
-		var result = new List<int>();
-		var sortedArrayAsStack = new Stack<int>(array.OrderBy(num => num).ToArray());
-		result.Add(sortedArrayAsStack.Pop());
-		result.Add(sortedArrayAsStack.Pop());
-		result.Add(sortedArrayAsStack.Pop());
-		return result.OrderBy(num => num).ToArray();
-	}
-}
+// public class Program {
+// 	public static int[] FindThreeLargestNumbers(int[] array) {
+// 		var result = new List<int>();
+// 		var sortedArrayAsStack = new Stack<int>(array.OrderBy(num => num).ToArray());
+// 		result.Add(sortedArrayAsStack.Pop());
+// 		result.Add(sortedArrayAsStack.Pop());
+// 		result.Add(sortedArrayAsStack.Pop());
+// 		return result.OrderBy(num => num).ToArray();
+// 	}
+// }
 
 // https://www.algoexpert.io/questions/Find%20Three%20Largest%20Numbers
