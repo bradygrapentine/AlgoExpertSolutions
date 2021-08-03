28a45d954032ad3ed445669ea26037acdba10859
Brady Grapentine <82343457+bradygrapentine@users.noreply.github.com>
2021-08-03 13:35:29 -0400

    Update Easy_InsertionSort.cs

--- Easy_InsertionSort.cs
+++ Easy_InsertionSort.cs
@@ -1,10 +1,14 @@
 public class Program {
 	public static int[] InsertionSort(int[] array) {
+		
 		var end = 1;
-		while (end < array.Length) {
-			for (var current = end; current > 0; current--) {
+		while (end < array.Length) 
+		{
+			for (var current = end; current > 0; current--) 
+			{
 				var currentDuplicate = array[current];
-				if (array[current] < array[current-1]){
+				if (array[current] < array[current-1]) 
+				{
 					array[current] = array[current-1];
 					array[current-1] = currentDuplicate;
 				}
@@ -12,6 +16,7 @@ public class Program {
 			end++;
 		}
 		return array;
+	
 	}
 }
 
