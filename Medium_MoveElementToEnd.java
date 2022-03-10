c640eefda42aaacc2913ed0b39c7116d8e5441c1
Brady Grapentine <82343457+bradygrapentine@users.noreply.github.com>
2022-03-09 21:58:15 -0500

    Create Medium_MoveElementToEnd.java

---
+++ Medium_MoveElementToEnd.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import static java.util.stream.Collectors.toList;
+
+class Program {
+  public static List<Integer> moveElementToEnd(List<Integer> array, int toMove) {
+		List<Integer> filteredArray =  array
+			                             .stream()
+                                   .filter(t -> t != toMove)
+                                   .collect(toList());
+		int count = array.size() - filteredArray.size();
+		for (int i = 0; i < count; i++) {
+			filteredArray.add(toMove);
+		}
+		return filteredArray;
+  }
+}
+
